2b7a44f5b0e4b9215ec602ecbc55f8d65f25f87a
funcoders00-vibe <funcoders00@gmail.com>
2026-06-17 22:33:28 +0530

    db config changes

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -9,8 +9,6 @@ class Config:
     db_port: str
     db_host: str
     db_name: str
-    db_username: str
-    db_password: str
     port: int
     host: str
     log_level: str
@@ -22,8 +20,6 @@ def get_config():
         db_port=os.getenv('DB_PORT'),
         db_host=os.getenv('DB_HOST'),
         db_name=os.getenv('DB_NAME'),
-        db_username=os.getenv('DB_USERNAME',),
-        db_password=os.getenv('DB_PASSWORD'),
         port=int(os.getenv('PORT')),
         host=os.getenv('HOST'),
         log_level=os.getenv('LOG_LEVEL'),
